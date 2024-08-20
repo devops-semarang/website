5721c465e63504133e735f3aeac4b390bca819ce
Abdur Rofi <rofim58@gmail.com>
2024-08-20 14:12:52 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Events } from "./pages/events";
+import { Index } from "./pages";
 
 function App() {
   return (
