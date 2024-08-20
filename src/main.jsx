3eaf2c101d5121a7054cef729da93fc8f36aa999
Abdur Rofi <rofim58@gmail.com>
2024-08-20 13:26:41 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <head>
+      <meta
+        name="facebook-domain-verification"
+        content="g143tc98a7u9ssgylzc1py2syne66n"
+      />
+    </head>
     <App />
   </StrictMode>,
-)
+);
